7b1b54601864115b13e0b27b1791004703cb84f1
c4chamok <c4chamok@gmail.com>
2024-09-25 09:40:58 +0600

    donate button script added

--- JS/UI.js
+++ JS/UI.js
@@ -3,7 +3,7 @@ const donationTab = document.getElementById("donation-tab");
 const historyBtn = document.getElementById("history-btn");
 const donationTabBtn = document.getElementById("donationtab-btn");
 
-
+const donateBtns = document.getElementsByClassName("donate-btn");
 
 document.getElementById("swap-btn").addEventListener("click",function(event){
 
@@ -27,6 +27,26 @@ document.getElementById("swap-btn").addEventListener("click",function(event){
 })
 
 
+
+for(let i = 0;i<=donateBtns.length;i++){
+    donateBtns[i].addEventListener("click",function(){
+        
+        const input = donationTab.children[i].getElementsByTagName("input")[0];
+        const inputValueNum = parseFloat(input.value);
+
+        const getBalance = document.getElementById("balance");
+        const balanceNum = parseFloat(getBalance.innerText);
+
+        if(!isNaN(inputValueNum) && inputValueNum >= 0 && inputValueNum <= balanceNum){
+            displayDonationAmount(i,inputValueNum);
+            displayBalance(inputValueNum);
+            
+        }
+        else{
+            alert("Invalid input or Insufficient Balance.")
+        }
+    });
+}
 
 
 
