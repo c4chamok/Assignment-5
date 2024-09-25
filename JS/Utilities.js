a55959e213e9d5679bfc3075c7863567d69b9af9
c4chamok <c4chamok@gmail.com>
2024-09-25 09:37:08 +0600

    Function added for display donation amount

--- JS/Utilities.js
+++ JS/Utilities.js
@@ -3,3 +3,16 @@ function displayBalance(balance){
     let balanceNum = parseFloat(getBalance.innerText);
     getBalance.innerText = balanceNum - balance;
 }
+
+function displayDonationAmount(section,amount){
+    let currentAmmount = document.getElementsByClassName("donated-amount")[section];
+    let currentAmmountNum = parseFloat(currentAmmount.innerText)
+    currentAmmount.innerText = `${amount+currentAmmountNum} BDT`;
+}
+
+
+
+
+
+
+
